d91ae62bd6a486b80ab2c4f3d694e1e1536321cb
p15823230419-prog <admin@example.com>
2025-11-25 17:33:50 +0800

    010

--- project/dao/user_dao.go
+++ project/dao/user_dao.go
@@ -6,7 +6,6 @@ import (
 	"abc/model"
 	"context"
 	"errors"
-	"fmt"
 	"log"
 
 	"github.com/gin-gonic/gin"
@@ -47,43 +46,37 @@ func (u *UserDao) FindById(ctx context.Context, id uint64) (*entity.User, error)
 func (u *UserDao) FindByUsername(ctx context.Context, username string) (*entity.User, error) {
 	var modelUser model.User
 	err := db.WithContext(ctx).Where("username = ?", username).First(&modelUser).Error
-	if err != nil && !errors.Is(err, gorm.ErrRecordNotFound) {
-		return nil, err
-	} else if errors.Is(err, gorm.ErrRecordNotFound) {
+	if errors.Is(err, gorm.ErrRecordNotFound) {
 		return nil, nil
+	}
+	if err != nil {
+		return nil, err
 	}
 	return userModelToEntity(modelUser), nil
 }
 
 // 获取用户列表
 func (u *UserDao) FindUsers(ctx context.Context, username string, pageNum int, pageSize int) ([]*entity.User, error) {
+	var modelUsers []model.User
+
+	query := db.WithContext(ctx).
+		Model(&model.User{}).
+		Preload("Roles")
+
 	if username != "" {
-		var modelUsers []model.User
-		if err := db.WithContext(ctx).
-			Model(model.User{}).
-			Preload("Roles").
-			Where("username like ?", "%"+username+"%").
-			Limit(pageSize).
-			Offset((pageNum - 1) * pageSize).
-			Find(&modelUsers).
-			Error; err != nil {
-			return nil, err
-		}
-		return userModelsToEntities(modelUsers), nil
-	} else {
-		var modelUsers []model.User
-		if err := db.WithContext(ctx).
-			Model(model.User{}).
-			Preload("Roles").
-			Limit(pageSize).
-			Offset((pageNum - 1) * pageSize).
-			Find(&modelUsers).
-			Error; err != nil {
-			return nil, err
-		}
-		fmt.Println(modelUsers)
-		return userModelsToEntities(modelUsers), nil
+		query = query.Where("username LIKE ?", "%"+username+"%")
 	}
+
+	err := query.
+		Limit(pageSize).
+		Offset((pageNum - 1) * pageSize).
+		Find(&modelUsers).Error
+
+	if err != nil {
+		return nil, err
+	}
+
+	return userModelsToEntities(modelUsers), nil
 }
 
 // 删除用户
